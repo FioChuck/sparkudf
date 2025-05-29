2438c1ab7e928232070803d7284af9a3548f4565
chasf <chasfiorenza@gmail.com>
2025-05-29 19:58:32 +0000

    print executor

--- src/main/java/com/example/serverless/sparkudf/HttpServiceUDF.java
+++ src/main/java/com/example/serverless/sparkudf/HttpServiceUDF.java
@@ -1,12 +1,23 @@
 package com.example.serverless.sparkudf;
 
-import com.example.serverless.sparkudf.WebhookService;
 import org.apache.spark.sql.api.java.UDF0;
+import org.apache.spark.TaskContext;
+
 
 public class HttpServiceUDF implements UDF0<String> {
 
     @Override
     public String call() throws Exception {
+
+        TaskContext tc = TaskContext.get();
+
+        String executorLog = "UDF EXECUTION CONFIRMED -> Stage: " + tc.stageId() +
+                ", Partition: " + tc.partitionId() +
+                ", Task Attempt: " + tc.taskAttemptId();
+
+        // This will print to the executor's stdout log
+        System.out.println(executorLog);
+
         WebhookService service = new WebhookService();
         return service.getPublicIpAddress();
     }
